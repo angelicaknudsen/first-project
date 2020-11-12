c9210b7aeb6ec81bc36aa8826ba4de215ebd7763
angelicaknudsen <eatingoreosrightnow@gmail.com>
2020-11-12 12:47:27 -0600

    1st attempt at commit through fork plz work

---
+++ src/main.cpp
@@ -0,0 +1,147 @@
+#include "main.h"
+
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+
+	pros::lcd::register_btn1_cb(on_center_button);
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+void opcontrol() {
+	using namespace pros;
+
+	Motor driveFrontLeft(16);
+	Motor driveFrontRight(9, true);
+	Motor driveBackLeft(17);
+	Motor driveBackRight(10, true);
+
+	Motor uptakeLow(14);
+	Motor uptakeHigh(15);
+
+	Motor intakeRight(1);
+	Motor intakeLeft(2, true);
+
+	Controller joystick(CONTROLLER_MASTER);
+
+	while (true) {
+		int left,right;
+		int low, high;
+		int iForward, iReverse;
+
+		left = joystick.get_analog(ANALOG_LEFT_Y);
+		right = joystick.get_analog(ANALOG_RIGHT_Y);
+
+		low = joystick.get_digital(DIGITAL_R2);
+		high = joystick.get_digital(DIGITAL_R1);
+
+		iForward = joystick.get_digital(DIGITAL_L1);
+		iReverse = joystick.get_digital(DIGITAL_L2);
+
+		/*driveFrontLeft.move(left);
+		driveBackLeft.move(left);
+
+		driveFrontRight.move(right);
+		driveBackRight.move(right);*/
+
+		driveFrontLeft = left;
+		driveBackLeft = left;
+		driveFrontRight = right;
+		driveBackRight = right;
+
+		if (low) {
+			uptakeHigh = -high;
+			uptakeLow = low;
+		}
+		else if (high) {
+			uptakeHigh = high;
+			uptakeLow = low;
+		}
+		else {
+			uptakeHigh = 0;
+			uptakeLow = 0;
+		}
+
+		if (iForward) {
+			intakeLeft = iForward;
+			intakeRight = iForward;
+		}
+		else if (iReverse) {
+			intakeLeft = iReverse;
+			intakeRight = iReverse;
+		}
+		else {
+			intakeLeft = 0;
+			intakeRight = 0;
+		}
+
+		delay(20); //20 milliseconds.. helps cool CPU
+	}
+
+}
